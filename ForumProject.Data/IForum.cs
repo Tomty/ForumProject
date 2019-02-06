866c53e6c5bfbc6876f398c713f1de8f707c5c30
Tomty Tomty <thomas.wirth.2@gmail.com>
2019-02-06 18:13:17 +0100

    Adding service layer

---
+++ ForumProject.Data/IForum.cs
@@ -0,0 +1,21 @@
+﻿using ForumProject.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForumProject.Data
+{
+    public interface IForum
+    {
+        Forum GetById(int id);
+        IEnumerable<Forum> GetAll();
+        IEnumerable<ApplicationUser> GetAllActiveUsers();
+
+        Task Create(Forum forum);
+        Task Delete(int id);
+        Task UpdateTitle(int forumId, string newTitle);
+        Task UpdateDescription(int forumId, string newDescription);
+
+    }
+}
---
+++ ForumProject.Service/ForumService.cs
@@ -0,0 +1,55 @@
+﻿using ForumProject.Data;
+using ForumProject.Data.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace ForumProject.Service
+{
+    public class ForumService : IForum
+    {
+        ApplicationDbContext _context;
+
+        public ForumService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public Task Create(Forum forum)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<Forum> GetAll()
+        {
+            return _context.Forums
+                .Include(forum => forum.Posts); 
+        }
+
+        public IEnumerable<ApplicationUser> GetAllActiveUsers()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Forum GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateDescription(int forumId, string newDescription)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task UpdateTitle(int forumId, string newTitle)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}

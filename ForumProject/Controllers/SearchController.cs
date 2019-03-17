cee8a990c4e6a4a3e26099e4a06e0f51b6824c7e
Tomty Tomty <thomas.wirth.2@gmail.com>
2019-03-17 16:55:08 +0100

    update post service, create Search MVC

---
+++ ForumProject/Controllers/SearchController.cs
@@ -0,0 +1,74 @@
+﻿using ForumProject.Data;
+using ForumProject.Data.Models;
+using ForumProject.Models.Forum;
+using ForumProject.Models.Post;
+using ForumProject.Models.Search;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Linq;
+
+namespace ForumProject.Controllers
+{
+    public class SearchController : Controller
+    {
+        private readonly IPost _postService;
+
+        public SearchController(IPost postService)
+        {
+            _postService = postService;
+        }
+
+        public IActionResult Index(int id)
+        {
+            return View();
+        }
+
+        //public IActionResult Results(string searchQuery)
+        //{
+        //    var posts = _postService.GetFilteredPosts(searchQuery);
+
+        //    var areNoResults = (!string.IsNullOrEmpty(searchQuery) && !posts.Any());
+
+        //    var postListings = posts.Select(p => new PostListingModel
+        //    {
+        //        Id = p.Id,
+        //        AuthorId = p.User.Id,
+        //        AuthorName = p.User.UserName,
+        //        AuthorRating = p.User.Rating,
+        //        Title = p.Title,
+        //        DatePosted = p.Created.ToString(),
+        //        RepliesCount = p.PostReply.Count(),
+        //        Forum = BuildForumListing(p)
+        //    });
+
+        //    var model = new SearchResultModel
+        //    {
+        //        Posts = postListings,
+        //        SearchQuery = searchQuery,
+        //        EmptySearchResults = areNoResults
+
+        //    };
+        //}
+
+        private ForumListingModel BuildForumListing(Post p)
+        {
+            var forum = p.Forum;
+
+            return new ForumListingModel
+            {
+                Id = forum.Id,
+                ImageUrl = forum.ImageUrl,
+                Name = forum.Title,
+                Description = forum.Description
+            };
+        }
+
+        [HttpPost]
+        public IActionResult Search(string searchQuery)
+        {
+            return RedirectToAction("Results", new { searchQuery });
+        }
+    }
+
+        
+}

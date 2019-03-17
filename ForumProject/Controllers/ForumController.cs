7c49a89d60f161563c25347f9639a80190f28409
Tomty Tomty <thomas.wirth.2@gmail.com>
2019-03-17 16:27:05 +0100

    Fix Forum Search

--- ForumProject/Controllers/ForumController.cs
+++ ForumProject/Controllers/ForumController.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using ForumProject.Data;
 using ForumProject.Data.Models;
@@ -11,11 +12,12 @@ namespace ForumProject.Controllers
     public class ForumController : Controller
     {
         private readonly IForum _forumService;
-        //private readonly IPost _postService;
+        private readonly IPost _postService;
 
-        public ForumController(IForum forumService)
+        public ForumController(IForum forumService, IPost postService)
         {
             _forumService = forumService;
+            _postService = postService;
         }
 
         public IActionResult Index()
@@ -35,12 +37,17 @@ namespace ForumProject.Controllers
             return View(model);
         }
 
-        public IActionResult Topic(int id)
+        public IActionResult Topic(int id, string searchQuery)
         {
             var forum = _forumService.GetById(id);
-
-            //var posts = _postService.GetPostsByForum(id);
-            var posts = forum.Posts;
+            var posts = new List<Post>();
+            
+            if(!String.IsNullOrEmpty(searchQuery))
+            {
+                posts = _postService.GetFilteredPosts(forum, searchQuery).ToList();
+            }
+            else
+                posts = forum.Posts.ToList();
 
             var postListings = posts.Select(post => new PostListingModel {
                 Id = post.Id,
@@ -60,6 +67,12 @@ namespace ForumProject.Controllers
             };
 
             return View(model);
+        }
+
+        [HttpPost]
+        public IActionResult Search(int id, string searchQuery)
+        {
+            return RedirectToAction("Topic", new { id, searchQuery });
         }
 
         private ForumListingModel BuildForumListing(Post post)
